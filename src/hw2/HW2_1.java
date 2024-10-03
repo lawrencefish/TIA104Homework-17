d10b2b190f50d69a476326b6856d95307de8c0d0
Tom <lawrencefish35@gmail.com>
2024-10-03 19:31:06 +0800

    finish second homework

---
+++ src/hw2/HW2_1.java
@@ -0,0 +1,54 @@
+package hw2;
+
+import java.util.Scanner;
+
+public class HW2_1 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入三個數字: ");
+		double n1 = check(sc);
+		double n2 = check(sc);
+		double n3 = check(sc);
+
+		// 檢查是否為三角形
+		if ((n1 * n2 * n3 != 0) && ((n1 + n2) > n3) && (n2 + n3 > n1) && (n3 + n1 > n2)) {
+			if (n1 == n2 && n2 == n3) {
+				System.out.println("正三角形");
+			} else if (rightTriangle(n1, n2, n3)) {
+				System.out.println("直角三角形");
+			} else if (n1 == n2 || n2 == n3 || n1 == n3) {
+				System.out.println("等腰三角形");
+			} else {
+				System.out.println("其他三角形");
+			}
+		} else {
+			System.out.println("不是三角形");
+		}
+	}
+
+	// check 方法：輸入INT或DOUBLE都指定給DOUBLE
+	public static double check(Scanner sc) {
+		double deta = 0;
+		if (sc.hasNextInt()) {
+			deta = sc.nextInt();
+		} else if (sc.hasNextDouble()) {
+			deta = sc.nextDouble();
+		} else {
+			System.out.println("輸入的不是有效數字，請輸入數字。");
+		}
+		return deta;
+	}
+
+	// rightTriangle方法，檢查是否為直角
+	public static boolean rightTriangle(double a, double b, double c) {
+		double max = Math.max(a, Math.max(b, c));
+		if (max == a) {
+			return (b * b + c * c) == (a * a);
+		} else if (max == b) {
+			return (a * a + c * c) == (b * b);
+		} else {
+			return (a * a + b * b) == (c * c);
+		}
+	}
+}
---
+++ src/hw2/HW2_3.java
@@ -0,0 +1,38 @@
+package hw2;
+
+import java.util.Scanner;
+
+public class HW2_3 {
+//	阿文很喜歡簽大樂透(1～49)，但他是個善變的人，上次討厭數字是4，但這次他想要依心情決定討
+//	厭哪個數字，請您設計一隻程式，讓阿文可以輸入他不想要的數字(1～9)，畫面會顯示他可以選擇
+//	的號碼與總數，如圖：
+	public static void main(String[] args) {
+		// 阿文很熱衷大樂透(1 ～ 49)，但他不喜歡有4的數字，不論是個位數或是十位數。
+		// 請設計一隻程式，輸出結果為阿文可以選擇的數字有哪些？總共有幾個？
+		Scanner sc = new Scanner(System.in);
+		int num = 0, p = 0;
+		System.out.println("請輸入1 ~ 9的正整數");
+		while (true) {
+			if (sc.hasNextInt()) {
+				num = sc.nextInt();
+				if (num >= 1 && num <= 9) {
+					break;
+				} else {
+					num = 0;
+					System.out.println("請輸入1 ~ 9的正整數");
+				}
+			} else {
+				System.out.println("請輸入1 ~ 9的正整數");
+				continue;
+			}
+		}
+		for (int i = 1; i <= 49; i++) {
+			if (i % 10 != num && i / 10 != num) {
+				System.out.print(i + " ");
+				p++;
+			}
+		}
+		System.out.println();
+		System.out.println("總共有" + p + "個");
+	}
+}
