e75fb5712dd2bd995ad5970c41322fc3122fc668
Tom <lawrencefish35@gmail.com>
2024-10-05 15:47:17 +0800

    HW4 finish

--- src/hw2/HW2_1.java
+++ src/hw2/HW2_1.java
@@ -1,54 +1,86 @@
 package hw2;
 
-import java.util.Scanner;
-
 public class HW2_1 {
 	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		System.out.println("請輸入三個數字: ");
-		double n1 = check(sc);
-		double n2 = check(sc);
-		double n3 = check(sc);
-
-		// 檢查是否為三角形
-		if ((n1 * n2 * n3 != 0) && ((n1 + n2) > n3) && (n2 + n3 > n1) && (n3 + n1 > n2)) {
-			if (n1 == n2 && n2 == n3) {
-				System.out.println("正三角形");
-			} else if (rightTriangle(n1, n2, n3)) {
-				System.out.println("直角三角形");
-			} else if (n1 == n2 || n2 == n3 || n1 == n3) {
-				System.out.println("等腰三角形");
-			} else {
-				System.out.println("其他三角形");
+		//請設計一隻Java程式，計算1～1000的偶數和(2+4+6+8+…+1000)
+		int sum = 0;
+		for(int i = 2 ; i <= 1000 ; i += 2) {
+			sum += i;
+		}
+		System.out.println("1～1000的偶數和=" + sum);
+		
+		System.out.println("========================================");
+		//請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用for迴圈)
+		int pdt = 1;
+		for(int i = 1 ; i <= 10 ; i++) {
+			pdt = pdt * i;
+		}
+		System.out.println("1～10的連乘積=" + pdt);
+		
+		System.out.println("========================================");
+		//請設計一隻Java程式，計算1～10的連乘積(1*2*3*…*10) (用while迴圈)
+		int j = 1, k = 1;
+		while(j <= 10) {
+			k = k * j;
+			j++;
+		}
+		System.out.println("1～10的連乘積=" + k);
+		
+		System.out.println("========================================");
+		//請設計一隻Java程式，輸出結果為以下：
+		//1 4 9 16 25 36 49 64 81 100
+		for(int i = 1 ; i <= 10 ; i++) {
+			System.out.print(i * i + " ");
+		}
+		System.out.println();
+		
+		System.out.println("========================================");
+		//阿文很熱衷大樂透(1 ～ 49)，但他不喜歡有4的數字，不論是個位數或是十位數。
+		//請設計一隻程式，輸出結果為阿文可以選擇的數字有哪些？總共有幾個？
+		int p = 0;
+		for(int i = 1 ; i <= 49 ; i++) {
+			if(i % 10 != 4 && i/10 != 4) {
+				System.out.print(i + " ");
+				p++;
 			}
-		} else {
-			System.out.println("不是三角形");
 		}
-	}
-
-	// check 方法：輸入INT或DOUBLE都指定給DOUBLE
-	public static double check(Scanner sc) {
-		double deta = 0;
-		if (sc.hasNextInt()) {
-			deta = sc.nextInt();
-		} else if (sc.hasNextDouble()) {
-			deta = sc.nextDouble();
-		} else {
-			System.out.println("輸入的不是有效數字，請輸入數字。");
+		System.out.println();
+		System.out.println("總共有" + p + "個");
+		
+		System.out.println("========================================");
+//		請設計一隻Java程式，輸出結果為以下：
+//		1 2 3 4 5 6 7 8 9 10
+//		1 2 3 4 5 6 7 8 9
+//		1 2 3 4 5 6 7 8
+//		1 2 3 4 5 6 7
+//		1 2 3 4 5 6
+//		1 2 3 4 5
+//		1 2 3 4
+//		1 2 3
+//		1 2
+//		1
+		for(int x = 10 ; x >= 1 ; x--) {
+			for(int y = 1 ; y <= x ; y++) {
+				System.out.print(y + " ");
+			}
+			System.out.println();
 		}
-		return deta;
-	}
-
-	// rightTriangle方法，檢查是否為直角
-	public static boolean rightTriangle(double a, double b, double c) {
-		double max = Math.max(a, Math.max(b, c));
-		if (max == a) {
-			return (b * b + c * c) == (a * a);
-		} else if (max == b) {
-			return (a * a + c * c) == (b * b);
-		} else {
-			return (a * a + b * b) == (c * c);
+		
+		System.out.println("========================================");
+//		請設計一隻Java程式，輸出結果為以下：
+//		A
+//		BB
+//		CCC
+//		DDDD
+//		EEEEE
+//		FFFFFF
+//		使用Unicode，'A'=(char)65, 'B'=(char)66, 'C'=(char)67...
+		for(int i = 1 ; i <= 6 ; i++) {
+			for(int h = 1; h <= i ; h++) {
+				System.out.print((char)(i+64));
+			}
+			System.out.println();
 		}
 	}
 }
+
--- src/hw3/HW3_2.java
+++ src/hw3/HW3_2.java
@@ -1,8 +1,8 @@
-package hw2;
+package hw3;
 
 import java.util.Scanner;
 
-public class HW2_2 {
+public class HW3_2 {
 //	請設計一隻程式，會亂數產生一個0～9的數字，然後可以玩猜數字遊戲，猜錯會顯示錯誤訊息，猜
 //	對則顯示正確訊息，如圖示結果：
 //	進階功能：產生0～100亂數，每次猜就會提示你是大於還是小於正確答案)
