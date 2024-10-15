d11a1a1023894ba02e085bff7759e9387166b3c0
Tom <lawrencefish35@gmail.com>
2024-10-15 21:31:10 +0800

    HW6

--- src/hw6/CalTest.java
+++ src/hw6/CalTest.java
@@ -6,22 +6,20 @@ public class CalTest {
 	public static void main(String[] args) throws CalException {
 		Calculator c = new Calculator();
 		Scanner sc = new Scanner(System.in);
-		int x = 0;
-		int y = 0;
 
 		System.out.println("請輸入x");
 		while (!sc.hasNextInt()) {
 			System.out.println("輸入格式不正確，請重新輸入x");
 			sc.next();
 		}
-		x = sc.nextInt();
+		int x = sc.nextInt();
 
 		System.out.println("請輸入y");
 		while (!sc.hasNextInt()) {
 			System.out.println("輸入格式不正確，請重新輸入y");
 			sc.next();
 		}
-		y = sc.nextInt();
+		int y = sc.nextInt();
 
 		try {
 			System.out.println(c.powerXY(x, y));
